e923883ca3ceff4dacb4cbe521b5b6232ef628dd
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-08-24 14:00:24 -0300

    feat alterando as rotas

--- SecureTech/site/src/controllers/pythonController.js
+++ SecureTech/site/src/controllers/pythonController.js
@@ -1,11 +1,11 @@
 var pythonModel = require("../models/pythonModel");
-function calcularGraph(req, res) {
+function ultDados(req, res) {
 
 
-    console.log(`Recuperando medidas em tempo real`);
+    console.log(`Recuperando os ultimos dados`);
 
 
-    pythonModel.calcularGraph().then(function (resultado) {
+    pythonModel.ultDados().then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -17,6 +17,29 @@ function calcularGraph(req, res) {
         res.status(500).json(erro.sqlMessage);
     });
 }
+
+function realTime(req, res) {
+
+    
+    console.log(`Recuperando medidas em tempo real`);
+
+    pythonModel.ultDados().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os ultimos dados.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+
+
+
 module.exports = {
-    calcularGraph
+    ultDados,
+    realTime
 }
--- SecureTech/site/src/routes/python.js
+++ SecureTech/site/src/routes/python.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 var usuarioController = require("../controllers/pythonController");
 
-router.get("/calcularGraph", function (req, res) {
-    pythonController.calcularGraph(req, res);
+router.get("/ultDados", function (req, res) {
+    pythonController.ultDados(req, res);
 });
+
+
+router.get("/realTime/", function (req, res) {
+    pythonController.relTime(req, res);
+})
 
 module.exports = router;
