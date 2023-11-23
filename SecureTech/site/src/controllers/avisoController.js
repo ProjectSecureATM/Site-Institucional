25c2d6045f707c699c244c91877fb3795efe0f34
Zuull21 <julio.dahi@sptech.school>
2023-11-23 19:37:33 -0300

    Alterações Alerts

--- SecureTech/site/src/controllers/avisoController.js
+++ SecureTech/site/src/controllers/avisoController.js
@@ -1,10 +1,69 @@
 var avisoModel = require("../models/avisoModel");
 
-function listar(req, res) {
+function Alertas(req, res) {
 
     var fkEmpresa =  req.params.fkEmpresa;
 
-    avisoModel.listar().then(function (resultado) {
+    avisoModel.coletaAlertaCpuAmarelo().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+
+    avisoModel.coletaAlertaCpuVermelho().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+
+    avisoModel.coletaAlertaRamAmarelo().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+
+    avisoModel.coletaAlertaRamVermelho().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+    
+    avisoModel.coletaAlertaDiscoVermelho().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+    avisoModel.coletaAlertaDiscoAmarelo().then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -18,5 +77,5 @@ function listar(req, res) {
 }
 
 module.exports = {
-    listar
+    Alertas
 }
--- SecureTech/site/src/models/avisoModel.js
+++ SecureTech/site/src/models/avisoModel.js
@@ -1,13 +1,60 @@
 var database = require("../database/config");
 
-function listar(fkEmpresa) {
+function coletaAlertaRamAmarelo(fkEmpresa) {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-    select empresa.nome, agencia.idAgen, agencia.nAgencia from agencia join empresa on fkEmpresa = ${idEmp} where idEmp = ${fkEmpresa};
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 1 AND Valor > 65 AND Valor < 80;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
-module.exports = {
-    listar
+function coletaAlertaCpuAmarelo(fkEmpresa) {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 3 AND Valor > 40 AND Valor < 55;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
 }
+function coletaAlertaDiscoAmarelo(fkEmpresa) {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 2 AND Valor > 70 AND Valor < 85;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+function coletaAlertaRamVer(fkhoEelmpresa) {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 1 AND Valor > =680
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+function coletaAlertaCpu(VermelhofkEmpresa) {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 3 AND Valor > =455
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+function coletaAlertaDisco(VermelhofkEmpresa) {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    SELECT * FROM leitura JOIN atm ON leitura.ATMComp_ID = ATM.idATM WHERE Componente_ID = 2 AND Valor > =785
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
+module.exports = {
+    ccoletaAlertaRamAmarelo,
+    coletaAlertaCpuAmarelo,
+    coletaAlertaDiscoAmarelo,
+    coletaAlertaRamVermelho,
+    coletaAlertaCpuVermelho,
+    coletaAlertaDiscoVermelho
+}m
