b56aa50e3a813a6cefbca410b324d1fa2d3221ae
PauloCafasso <paulo.cafasso@sptech.school>
2023-11-24 08:56:14 -0300

    BackEnd alertas concluido

--- SecureTech/site/src/controllers/avisoController.js
+++ SecureTech/site/src/controllers/avisoController.js
@@ -79,3 +79,36 @@ function Alertas(req, res) {
 module.exports = {
     Alertas
 }
+
+// var avisoModel = require("../models/avisoModel");
+
+// async function Alertas(req, res) {
+//     var fkEmpresa = req.params.fkEmpresa;
+
+//     try {
+//         const resultados = await Promise.all([
+//             avisoModel.coletaAlertaCpuAmarelo(),
+//             avisoModel.coletaAlertaCpuVermelho(),
+//             avisoModel.coletaAlertaRamAmarelo(),
+//             avisoModel.coletaAlertaRamVermelho(),
+//             avisoModel.coletaAlertaDiscoAmarelo(),
+//             avisoModel.coletaAlertaDiscoVermelho()
+//         ]);
+
+//         const hasResults = resultados.some(resultado => resultado.length > 0);
+
+//         if (hasResults) {
+//             res.status(200).json(resultados);
+//         } else {
+//             res.status(204).send("Nenhum resultado encontrado!");
+//         }
+//     } catch (erro) {
+//         console.log(erro);
+//         console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+//         res.status(500).json(erro.sqlMessage);
+//     }
+// }
+
+// module.exports = {
+//     Alertas
+// };
--- SecureTech/site/src/routes/avisos.js
+++ SecureTech/site/src/routes/avisos.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 var avisoController = require("../controllers/avisoController");
 
-router.get("/listar:fkEmpresa", function (req, res) {
-    avisoController.listar(req, res);
+router.get("/Alertas/:fkEmpresa", function (req, res) {
+    avisoController.Alertas(req, res);
 });
 
 module.exports = router;
