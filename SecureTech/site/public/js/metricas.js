4e7afbf54990532e24179f77298391bf120b4c4b
freitazgiovanna <giovanna.bezerra@sptech.school>
2023-11-27 23:16:33 -0300

    alterações na dashboard do projeto individual, adicionando as métricas de uma forma melhor

---
+++ SecureTech/site/public/js/metricas.js
@@ -0,0 +1,9 @@
+function notificarATM2() {
+    var modal2 = document.getElementById('modal2');
+    modal2.style.display = 'block';
+}
+
+function closeModal2() {
+    var modal2 = document.getElementById('modal2');
+    modal2.style.display = 'none';
+}
