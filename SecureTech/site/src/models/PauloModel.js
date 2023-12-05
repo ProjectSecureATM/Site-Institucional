22e9e431fee4a89bb226091a520dae53966cc0a0
PauloCafasso <paulo.cafasso@sptech.school>
2023-12-05 10:48:50 -0300

    atualização comandos paulo model

--- SecureTech/site/src/models/PauloModel.js
+++ SecureTech/site/src/models/PauloModel.js
@@ -2,12 +2,12 @@ var database = require("../database/config")
 const util = require('util');
 
 function buscarRamHora(idATM) {
-    // SELECT Valor,DataRegistro FROM leitura WHERE ATMComp_ID=1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM} order by LeituraID desc limit 10;`;
+    // SELECT Valor,DataRegistro FROM leitura WHERE ATMComp_ID=1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM} order by LeituraID desc limit 10;`;
 
     var instrucaoSql = `
     SELECT TOP 10 Valor, DataRegistro
     FROM leitura
-    WHERE ATMComp_ID = 1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM}
+    WHERE ATMComp_ID = 1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM}
     ORDER BY LeituraID DESC;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -16,10 +16,10 @@ function buscarRamHora(idATM) {
 }
 
 function buscarTempoReal(idATM) {
-    // SELECT Valor,DataRegistro FROM leitura WHERE ATMComp_ID=1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM} order by LeituraID desc limit 1;
+    // SELECT Valor,DataRegistro FROM leitura WHERE ATMComp_ID=1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM} order by LeituraID desc limit 1;
     var instrucaoSql = `SELECT TOP 1 Valor, DataRegistro
     FROM leitura
-    WHERE ATMComp_ID = 1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM}
+    WHERE ATMComp_ID = 1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM}
     ORDER BY LeituraID DESC;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -28,10 +28,10 @@ function buscarTempoReal(idATM) {
 }
 
 function buscarMaximo(idATM) {
-    // SELECT max(Valor) as Valor FROM leitura WHERE ATMComp_ID=1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};
+    // SELECT max(Valor) as Valor FROM leitura WHERE ATMComp_ID=1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};
     var instrucaoSql = `SELECT MAX(Valor) AS Valor
     FROM leitura
-    WHERE ATMComp_ID = 1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};`;
+    WHERE ATMComp_ID = 1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -39,10 +39,10 @@ function buscarMaximo(idATM) {
 }
 
 function buscarMinimo(idATM) {
-    // SELECT min(Valor) as Valor FROM leitura WHERE ATMComp_ID=1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};
+    // SELECT min(Valor) as Valor FROM leitura WHERE ATMComp_ID=1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = ${idATM};
     var instrucaoSql = `SELECT MIN(Valor) AS Valor
     FROM leitura
-    WHERE ATMComp_ID = 1 AND APIID = 3 AND Componente_ID = 1 AND ATMComp_ID = 1;`;
+    WHERE ATMComp_ID = 1 AND APIID = 4 AND Componente_ID = 1 AND ATMComp_ID = 1;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
