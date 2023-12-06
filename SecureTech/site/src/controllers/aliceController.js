609efa988053311ebc1528e54225b5a6dd90af65
PauloCafasso <paulo.cafasso@sptech.school>
2023-12-06 09:56:12 -0300

    visao geral alice funcional

--- SecureTech/site/src/controllers/aliceController.js
+++ SecureTech/site/src/controllers/aliceController.js
@@ -1,42 +1,82 @@
+
 var aliceModel = require('../models/aliceModel');
 
-function logTempoReal(req,res){
+function logTempoReal(req, res) {
 
     var idATM = req.params.idATM;
 
-aliceModel.logTempoReal(idATM).them(function(resultado){
-    if (resultado.length > 0){
-        res.status(200).json(resultado);;
-    } else {
-        req.status(204).send("Nenhum resultado encontrado.")
-    }
-}).catch(function(erro){
-    console.log(erro);
-    console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
-    res.status(500).json(erro.sqlMessage)
-})
+    aliceModel.logTempoReal(idATM).them(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);;
+        } else {
+            req.status(204).send("Nenhum resultado encontrado.")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage)
+    })
 
 }
 
-function comparacaoLogsSucessoEFalha(req,res){
+function comparacaoLogsSucessoEFalha(req, res) {
 
     var idATM = req.params.idATM;
 
-aliceModel.comparacaoLogsSucessoEFalha(idATM).them(function(resultado){
-    if (resultado.length > 0){
-        res.status(200).json(resultado);;
-    } else {
-        req.status(204).send("Nenhum resultado encontrado.")
-    }
-}).catch(function(erro){
-    console.log(erro);
-    console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
-    res.status(500).json(erro.sqlMessage)
-})
+    aliceModel.comparacaoLogsSucessoEFalha(idATM).them(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);;
+        } else {
+            req.status(204).send("Nenhum resultado encontrado.")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage)
+    })
 
 }
+
+function logDia(req, res) {
+
+    var idATM = req.params.idATM;
+
+    aliceModel.logDia(idATM).them(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);;
+        } else {
+            req.status(204).send("Nenhum resultado encontrado.")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage)
+    })
+
+}
+
+function logHora(req, res) {
+
+    var idATM = req.params.idATM;
+
+    aliceModel.logHora(idATM).them(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);;
+        } else {
+            req.status(204).send("Nenhum resultado encontrado.")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as últimas medidas", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage)
+    })
+
+}
+
 
 module.exports = {
     logTempoReal,
-    comparacaoLogsSucessoEFalha
+    comparacaoLogsSucessoEFalha,
+    logHora,
+    logDia
 }
--- SecureTech/site/src/models/aliceModel.js
+++ SecureTech/site/src/models/aliceModel.js
@@ -1,26 +1,58 @@
 var database = require("../database/config")
 
-function logTempoReal (idATM){
-    
+function logDia(idATM) {
+
     var instrucaoSql = `
-    SELECT idLogs, data_hora FROM logs
-        ORDER BY idLogs DESC 
-            LIMIT 10;`;
+    SELECT COUNT(idLogs) AS quantidade, FORMAT(data_hora, '%H:%i:%s') AS hora FROM logs where fk_idATM = ${idATM}
+				GROUP BY data_hora ORDER BY data_hora DESC;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function comparacaoLogsSucessoEFalha(idATM){
-    
+function logHora(idATM) {
+
     var instrucaoSql = `
-    SELECT Tipo, COUNT(*) AS qtdErros FROM TipoERRO GROUP BY Tipo;`;
+    SELECT FORMAT(data_hora, '%H:%i:%s') AS hora, COUNT(logs.idLogs) AS quantidadeERRO FROM logs
+        JOIN mensagem ON logs.idLogs = mensagem.fkLogs
+            WHERE logs.fk_idATM = ${idATM} AND mensagem.Mensagem LIKE '%não%'
+                GROUP BY data_hora ORDER BY data_hora;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function logTempoReal(idATM) {
+
+    var instrucaoSql = `
+    SELECT TOP 10 idLogs, data_hora FROM logs
+				WHERE fk_idATM = ${idATM}
+					ORDER BY idLogs DESC;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function comparacaoLogsSucessoEFalha(idATM) {
+
+    var instrucaoSql = `
+    SELECT COUNT(idTipoERRO) AS qtdErros, Tipo FROM TipoErro 
+				JOIN mensagem ON fkMSG = idMensagem
+					JOIN logs ON fkLogs = idLogs
+						JOIN ATM ON fk_idATM = idATM
+							WHERE fk_idATM = ${idATM} GROUP BY Tipo;
+                            `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
+    logDia,
+    logHora,
     logTempoReal,
     comparacaoLogsSucessoEFalha
 }
